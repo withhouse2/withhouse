a7a0441a080a12e27f6077baa2f67ce80b358b7b
withhouse2 <withhouse2@proton.me>
2026-08-26 22:10:33 -0600

    Update and rename data.js to ram.js

--- data.js
+++
@@ -1,5 +0,0 @@
-const data1="https://drive.proton.me/urls/PZKTWDZA78#FbHfb2T6CLYw";
-const data2="https://drive.proton.me/urls/6FMX812Z6M#gXG5Mwz7RgCG";
-const data3="https://drive.proton.me/urls/2DSBW9W1G0#bUZ4mLIDEpBp";
-
-console.log(data1);
---
+++ ram.js
@@ -0,0 +1,5 @@
+const ram1="https://drive.proton.me/urls/PZKTWDZA78#FbHfb2T6CLYw";
+const ram2="https://drive.proton.me/urls/6FMX812Z6M#gXG5Mwz7RgCG";
+const ram3="https://drive.proton.me/urls/2DSBW9W1G0#bUZ4mLIDEpBp";
+const ram4="https://drive.proton.me/urls/WK8HXWPR7G#D38D3lxYkRuZ";
+console.log(data1);
